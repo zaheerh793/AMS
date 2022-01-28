e83bf3d9f2b7353c48bc73481ab4b22bca6994cc
ZaheerHussain <72617664+zaheerh793@users.noreply.github.com>
2022-01-28 23:47:21 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+from tkinter import filedialog
+
+import LoginPage
+
+if __name__ == '__main__':
+     LoginPage.mainwindow()
+
+
+
+
+
+''' l=StudentsPage.StudentView().__int__("zaheer","zaheer hussain","Zaheer@gmail.com","03459356102","password")'''
+
+"""
+create database AMS
+
+CREATE TABLE Student (
+    username varchar(50)PRIMARY KEY,
+    NAMES varchar(50),
+    Email varchar(50),
+    contactNo varchar(50),
+    passwords varchar(50)
+);
+insert into Student(username,NAMES,Email,contactNo,passwords)values('zaheer55',
+'zaheer hussain','zaheer793@gmail.com','03480995181','password')
+select username,passwords from Studen"""
